792549da68621a13ed1dc48f50ed2f1bb1f5450f
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-01 15:04:35 +0200

    return route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -17,4 +17,23 @@ class MainController extends AbstractController
         var_dump("page principale");
         die;
     }
+
+    // Exercice : créer un route qui va afficher "Bienvenue sur notre super site".
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home()
+    {
+        // dd est la focntion dump and die qui permet de débugger sans avoir à utiliser die.
+        dd("Bienvenue sur notre site");
+    }
+
+    /**
+     * @Route("legal", name="legal")
+     */
+    public function legal()
+    {
+        return new Response("Voici les mentions légales du site");
+    }
 }
