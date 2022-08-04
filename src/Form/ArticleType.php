6f35e299914973520368d1691e4d755b76ea7e16
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-04 14:49:34 +0200

    champs tag et category

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +18,14 @@ class ArticleType extends AbstractType
         $builder
             ->add('title')
             ->add('content')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('tag', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name'
+            ])
             ->add('Enregistrer', SubmitType::class);
     }
 
