343ded2f0db8d70d9a6b3a7e4933083ca19b4242
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-05 09:24:48 +0200

    exo routes

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -13,19 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    /**
-     * @Route("/category", name="app_category")
-     */
+
     public function index(): Response
     {
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
         ]);
     }
-
-    /**
-     * @Route("categories", name="categories_list")
-     */                                 // autowire
+    //////////////////////////////////// autowire
     public function listCategories(CategoryRepository $categoryRepository)
     {
         // la méthode findAll récupère toutes les catégories enregistrées dans la base de données.
@@ -37,9 +32,6 @@ class CategoryController extends AbstractController
     // Exercice : faire une méthode qui affiche les tags (nom, description) 
     // et une méthode qui va afficher les articles (titre, contenu)
 
-    /**
-     * @Route("category/{id}", name="category_show")
-     */
     public function showCategory($id, CategoryRepository $categoryRepository)
     {
         // la méthode find permet de récupérer une catégorie en fonction de son id.
@@ -48,9 +40,6 @@ class CategoryController extends AbstractController
         return $this->render("category_show.html.twig", ['category' => $category]);
     }
 
-    /**
-     * @Route("update/category/{id}", name="update_category")
-     */
     public function updateCategory(
         $id,
         CategoryRepository $categoryRepository,
@@ -79,9 +68,6 @@ class CategoryController extends AbstractController
         return $this->render("category_form.html.twig", ['categoryForm' => $categoryForm->createView()]);
     }
 
-    /**
-     * @Route("create/category", name="create_category")
-     */
     public function createCategory(EntityManagerInterface $entityManagerInterface, Request $request)
     {
         $category = new Category();
@@ -103,10 +89,7 @@ class CategoryController extends AbstractController
         return $this->render('category_form.html.twig', ['categoryForm' => $categoryForm->createView()]);
     }
 
-    /**
-     * @Route("delete/category/{id}", name="delete_category")
-     */
-    public function deletCategory(
+    public function deleteCategory(
         $id,
         CategoryRepository $categoryRepository,
         EntityManagerInterface $entityManagerInterface
