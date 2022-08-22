3bbfd0c14404bccf29ad6ec58dd034b084da1a64
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-22 16:24:32 +0200

    querybuilder

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -21,6 +21,27 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
+    public function searchByTerm($term)
+    {
+        // Création du queryBuilder qui va permettre de faire la requête SQL
+        $queryBuilder = $this->createQueryBuilder("article");
+        $query = $queryBuilder
+            ->select('article') // On fait le select
+            ->leftJoin('article.category', 'category') // on fait un leftjoin
+            ->leftJoin('article.tag', 'tag')
+            ->where('article.title LIKE :term') // on fait un where
+            ->orWhere('article.content LIKE :term') // on fait un OR WHERE
+            ->orWhere('category.name LIKE :term')
+            ->orWhere('category.description LIKE :term')
+            ->orWhere('tag.name LIKE :term')
+            ->orWhere('tag.description LIKE :term')
+            ->orWhere('tag.color LIKE :term')
+            ->setParameter('term', '%' . $term . "%") // on paramètre le contenu de term et on sécurise la requête
+            ->getQuery(); // on récupère la requête
+
+        return $query->getResult(); // On récupère le résultat
+    }
+
     public function add(Article $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
@@ -39,28 +60,28 @@ class ArticleRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Article[] Returns an array of Article objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Article[] Returns an array of Article objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('a.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Article
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Article
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
