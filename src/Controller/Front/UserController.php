88c49d2cb00ee3688d2769eb4d7a384af0a1545d
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-22 13:53:17 +0200

    insert User

--- src/Controller/Front/UserController.php
+++ src/Controller/Front/UserController.php
@@ -2,9 +2,50 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    /**
+     * @Route("user/add", name="insert_user")
+     */
+    public function userInsert(
+        EntityManagerInterface $entityManagerInterface,
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasherInterface
+    ) {
+
+        $user = new User();
+
+        $userForm = $this->createForm(UserType::class, $user);
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $user->setRoles(["ROLE_USER"]);
+
+            // Récupérer le mot de passe
+            $plainPassword = $userForm->get('password')->getData();
+
+            // Hasher le mot de passe
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
+
+            // Attribution du mpot de passe au user
+            $user->setPassword($hashedPassword);
+
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+
+        return $this->render('front/user_add.html.twig', ['userForm' => $userForm->createView()]);
+    }
 }
