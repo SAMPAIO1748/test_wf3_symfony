f4266c05ad6f9b050e1b7a7f8b735fc56a06ae5d
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-22 15:27:56 +0200

    role login

--- src/Controller/Front/UserController.php
+++ src/Controller/Front/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Front;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -36,7 +37,7 @@ class UserController extends AbstractController
             // Hasher le mot de passe
             $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
 
-            // Attribution du mpot de passe au user
+            // Attribution du mot de passe au user
             $user->setPassword($hashedPassword);
 
             $entityManagerInterface->persist($user);
@@ -50,4 +51,46 @@ class UserController extends AbstractController
     }
 
     // Exercice : créer un méthode qui va permettre à l'utilisateur connecté de modifié son compte.
+
+    /**
+     * @Route("update/user", name="front_update_user")
+     */
+    public function updateUser(
+        EntityManagerInterface $entityManagerInterface,
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasherInterface,
+        UserRepository $userRepository
+    ) {
+
+        // Récupération du user connecté
+        $user_connect = $this->getUser();
+
+        // Récupération de l'identifieur du user car il n'a pas accées au méthode de l'entité User.php
+        // il faut donc retrouver ce user avec le UserRepository
+        $user_email = $user_connect->getUserIdentifier();
+
+        // Récupération du user qui vient de l'entité User.php
+        $user = $userRepository->findOneBy(['email' => $user_email]);
+
+        $userForm = $this->createForm(UserType::class, $user);
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+
+            $plainPassword = $userForm->get('password')->getData();
+
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
+
+            $user->setPassword($hashedPassword);
+
+            $entityManagerInterface->persist($user);
+
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute("app_login");
+        }
+
+        return $this->render("front/user_add.html.twig", ['userForm' => $userForm->createView()]);
+    }
 }
