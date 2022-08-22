e7000dce1ec2dd49b2f89475f470dbdadd0cc152
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-22 16:48:37 +0200

    methode search

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("search/", name="search")
+     */
+    public function search(Request $request, ArticleRepository $articleRepository)
+    {
+        // $requet permet de récupérer le contenu du champs du formulaire
+        // On utilise query car c'est un formulaire en get
+        // Si c'était un formaulire en post on utiliserai request, cela donnerai ceci :
+        // $term = $request->request->get('search');
+        $term = $request->query->get('search');
+
+        // utilisation de la méthode créée dans ArticleRepository
+        $articles = $articleRepository->searchByTerm($term);
+
+        return $this->render('front/search.html.twig', ['articles' => $articles, 'term' => $term]);
+    }
+}
