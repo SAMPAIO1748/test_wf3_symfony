d6512668e91eeb6257a94147952111decbcf6a38
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-08-23 10:16:32 +0200

    MediaType

--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Article;
 use App\Entity\Media;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,16 @@ class MediaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('src')
+            ->add('src', FileType::class, [
+                'mapped' => false
+            ])
             ->add('alt')
             ->add('title')
-            ->add('article')
-        ;
+            ->add('article', EntityType::class, [
+                'class' => Article::class,
+                'choice_label' => 'title'
+            ])
+            ->add('Enregistrer', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
